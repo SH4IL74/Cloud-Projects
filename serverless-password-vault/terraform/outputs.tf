5cc352a3e5142af723c8119d0892bede38150170
SH4IL74 <purohitshail@gmail.com>
2026-08-18 11:03:01 +0000

    Removed the project which was not necessary right now

--- serverless-password-vault/terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "api_endpoint" {
-  description = "Base API Gateway REST Endpoint URL"
-  value       = "http://localhost:4566/_aws/execute-api/${aws_api_gateway_rest_api.vault_api.id}/prod"
-}
-output "website_url" {
-  description = "Frontend S3 Static Website URL"
-  value       = "http://${aws_s3_bucket.frontend_bucket.id}.s3-website.localhost.localstack.cloud:4566"
-}
